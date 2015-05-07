b817527080dfa3eb3548661b2df42e0c18edbbc6
Gilles Piou <gilles.piou@gmail.com>
2015-05-07 14:59:45 +0800

    Support webkit browser

--- whimsicalRipple.js
+++ whimsicalRipple.js
@@ -7,9 +7,11 @@
         styleSheet,
         rippleCSS,
         rippleLightCSS,
-        rippleKeyframes;
+        rippleKeyframes,
+        rippleWebkitKeyframes;
 
       rippleCSS = [
+        '-webkit-animation: ripple 800ms ease-out;',
         'animation: ripple 800ms ease-out;',
         'background-color: rgba(0, 0, 0, 0.16);',
         'border-radius: 100%;',
@@ -31,11 +33,26 @@
         '}'
       ];
 
+      rippleWebkitKeyframes = [
+        '@-webkit-keyframes ripple {',
+          'to {',
+            '-webkit-transform: scale(2);',
+            'opacity: 0;',
+          '}',
+        '}'
+      ];
+
       document.head.appendChild(styleEl);
       styleSheet = styleEl.sheet;
       styleSheet.insertRule('.ripple-effect {' + rippleCSS.join('') + '}', 0);
       styleSheet.insertRule('.ripple-light .ripple-effect {' + rippleLightCSS  + '}', 0);
-      styleSheet.insertRule(rippleKeyframes.join(''), 0);
+
+      if (CSSRule.WEBKIT_KEYFRAMES_RULE) { // WebKit
+        styleSheet.insertRule(rippleWebkitKeyframes.join(''), 0);
+      }
+      else if (CSSRule.KEYFRAMES_RULE) { // W3C
+        styleSheet.insertRule(rippleKeyframes.join(''), 0);
+      }
     })
     .directive('ripple', function() {
       return {
@@ -44,6 +61,11 @@
           element[0].style.position = 'relative';
           element[0].style.overflow = 'hidden';
           element[0].style.userSelect = 'none';
+
+          element[0].style.msUserSelect = 'none';
+          element[0].style.mozUserSelect = 'none';
+          element[0].style.webkitUserSelect = 'none';
+
 
           function createRipple(evt) {
             var ripple = angular.element('<span class="ripple-effect animate">'),
@@ -55,7 +77,7 @@
             ripple[0].style.width = ripple[0].style.height = radius + 'px';
             ripple[0].style.left = left + 'px';
             ripple[0].style.top = top + 'px';
-            ripple.on('animationend', function() {
+            ripple.on('animationend webkitAnimationEnd', function() {
               this.remove();
             });
 
